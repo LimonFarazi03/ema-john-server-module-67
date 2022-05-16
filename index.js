707231925cd73af489f9c92f45d7bfd66cf26e1d
MD Limon Farazi <abidislamabir@gmail.com>
2022-05-16 19:45:20 +0600

    some update

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-  res.send('This is home')
-})
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xq6vo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.listen(port,()=>{
-  console.log(`Example is listining on http://localhost:${port}`)
-})
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("dbJohn1").collection("product");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Ema john is running");
+});
+
+app.listen(port, () => {
+  console.log(`Example is listining on http://localhost:${port}`);
+});
